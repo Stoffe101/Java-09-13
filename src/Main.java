bded85c3583bd92e42ebe0b793ed6ff5b4ca4ada
Skrra <cloofsweden@gmail.com>
2025-09-15 13:07:46 +0200

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
+import java.util.Random;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[6];
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int)(Math.random() * 100) + 1;
+
+            sum += numbers[i];
+
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
         }
+        Arrays.sort(numbers);
+        double average = (double) sum / numbers.length;
+
+        int length = numbers.length;
+
+        double median;
+        if (length % 2 == 0) {
+            median = (numbers[length / 2 - 1] + numbers[length / 2]) / 2.0;
+        } else {
+            median = numbers[length / 2];
+        }
+
+        System.out.println("The numbers are: " + Arrays.toString(numbers));
+        System.out.println("The sum of the numbers is: " + (sum));
+        System.out.println("The maximum number is: " + max);
+        System.out.println("The minimum number is: " + min);
+        System.out.println("The average is: " + average);
+        System.out.println("The median is: " + median);
+
     }
+
 }
